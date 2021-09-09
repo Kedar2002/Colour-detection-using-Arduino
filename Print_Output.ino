f9320b8d180e4c6876cf60ee225030092be59b89
Kedar2002 <86146917+Kedar2002@users.noreply.github.com>
2021-09-09 10:34:22 +0530

    Create Print_Output.ino

---
+++ Print_Output.ino
@@ -0,0 +1,12 @@
+void printData(void)
+{
+  Serial.print("red= ");
+  Serial.print(red);
+  Serial.print("   green= ");
+  Serial.print(grn);
+  Serial.print("   blue= ");
+  Serial.print(blu);
+  Serial.print (" - ");
+  Serial.print (color);
+  Serial.println (" detected!");
+}
